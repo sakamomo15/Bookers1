843823e83e14ab713e85a2aba9699a16468dfb61
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-23 13:20:48 +0000

    [Fix] update

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2024_02_23_121213) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "title"
   end
 
